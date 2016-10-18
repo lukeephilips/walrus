f2745506d96c20b18540a8e777f36713ec4863d4
Kevin Finley and Luke Philips <kevin@>
2016-10-18 10:36:22 -0700

    add buttons and colors

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,20 @@ $(document).ready(function() {
   $("span#slider").click(function() {
     $("img#sliding").slideToggle("slow");
   });
+  $("button.red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red");
+  });
+  $("button.yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow");
+  });
+  $("button.blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue");
+  });
+  $("button.pink").click(function() {
+    $("body").removeClass();
+    $("body").addClass("pink");
+  });
 });
